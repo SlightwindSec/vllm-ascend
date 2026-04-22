401c7423d55b3fdadc5d7afbf9cd8bb95a1ae37a
TmacAaron <yangyit139@gmail.com>
2026-04-22 21:35:51 +0800

    Add hy_v3 quantization support
    Signed-off-by: SlightwindSec <slightwindsec@gmail.com>

--- vllm_ascend/quantization/modelslim_config.py
+++ vllm_ascend/quantization/modelslim_config.py
@@ -75,6 +75,18 @@ packed_modules_model_mapping: dict[str, dict[str, list[str]]] = {
         "in_proj_ba": ["in_proj_b", "in_proj_a"],
         "experts": ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
     },
+    "hy_v3": {
+        "qkv_proj": [
+            "q_proj",
+            "k_proj",
+            "v_proj",
+        ],
+        "gate_up_proj": [
+            "gate_proj",
+            "up_proj",
+        ],
+        "experts": ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
+    },
     "deepseek_v2": {
         "gate_up_proj": ["gate_proj", "up_proj"],
         "experts": ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
